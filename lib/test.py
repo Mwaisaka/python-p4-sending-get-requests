af974351decd9dc041ef38271174c68233b1457e
Frank <frankkashere@gmail.com>
2024-03-08 12:54:09 +0300

    Completed code

---
+++ lib/test.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+# url="https://learn-co-curriculum.github.io/json-site-example/"
+url="https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+response=requests.get(url)
+
+json_content=json.loads(response.content)
+
+print(json.dumps(json_content, indent=4))
+
+# print(json_content)
